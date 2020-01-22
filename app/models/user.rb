230499b1f59da19dc04d2f8a4051ee9b18b9fdab
Fabio Urquiza <fabiorush@gmail.com>
2020-01-22 14:14:13 -0300

    Add association of articles with users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
+  has_many :articles
+  before_save { self.downcase! }
   validates :username, presence: true, 
                        uniqueness: { case_sensitive: false },
                        length: { minimum: 3, maximum: 25 }
