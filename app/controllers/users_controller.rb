174f8464db16e3b577b277c8c48098efd33c2d2a
Fabio Urquiza <fabiorush@gmail.com>
2020-01-23 10:43:10 -0300

    Add user show and edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -8,6 +12,20 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Alpha Blog, #{@user.username}!"
+      redirect_to articles_path
+    else
+      r ender 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
       redirect_to articles_path
     else
       r ender 'new'
